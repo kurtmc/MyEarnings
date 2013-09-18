6dcbc39b69c478833c36cf976f21cc82569b4717
Kurt McAlpine <kurtmcalpine@gmail.com>
2013-09-18 12:27:25 +1200

    Added an Employee class implementation will come later.

---
+++ src/com/mcalpinedevelopment/calculatepay/Employee.java
@@ -0,0 +1,69 @@
+package com.mcalpinedevelopment.calculatepay;
+
+public class Employee {
+	public Employee() {
+		
+	}
+	
+	public String name() {
+		return null;
+	}
+	
+	public double payRate() {
+		return 0;
+	}
+	
+	public double hours() {
+		return 0;		
+	}
+	
+	public double gross() {
+		return 0;
+		
+	}
+	
+	public double paye() {
+		return 0;
+		
+	}
+	
+	public double studentLoan() {
+		return 0;
+		
+	}
+	
+	public double kiwiSaver() {
+		return 0;
+		
+	}
+	
+	public double nett() {
+		return 0;
+		
+	}
+	public String printGross() {
+		return null;
+		
+	}
+	
+	public String printPaye() {
+		return null;
+		
+	}
+	
+	public String printStudentLoan() {
+		return null;
+		
+	}
+	
+	public String printKiwiSaver() {
+		return null;
+		
+	}
+	
+	public String printNett() {
+		return null;
+		
+	}
+
+}
