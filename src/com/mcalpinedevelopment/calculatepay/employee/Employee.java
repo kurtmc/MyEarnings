c46915d7cae78c4f20c1891288a7a86539472acd
Kurt McAlpine <kurtmcalpine@gmail.com>
2013-11-03 21:04:25 +1300

    Moved computation code into TaxCalculator

--- src/com/mcalpinedevelopment/calculatepay/employee/Employee.java
+++ src/com/mcalpinedevelopment/calculatepay/employee/Employee.java
@@ -6,8 +6,10 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 
 import com.mcalpinedevelopment.calculatepay.Calculator;
-import com.mcalpinedevelopment.calculatepay.FileReader;
 import com.mcalpinedevelopment.calculatepay.R;
+import com.mcalpinedevelopment.calculatepay.database.EmployeeDatabase;
+import com.mcalpinedevelopment.calculatepay.database.EmployeePreferences;
+import com.mcalpinedevelopment.calculatepay.tax.TaxCalculator;
 
 import android.app.Activity;
 import android.content.Context;
@@ -18,8 +20,9 @@ public class Employee {
 	private double _hours;
 	private double _rate;
 	private String _paytype;	
-	private double[] _taxData;
-	private String[] _pay;
+	private TaxCalculator _taxCalculator;
+//	private double[] _taxData;
+//	private String[] _pay;
 	
 	public Employee(String message, Context context) {
 		_context = context;
@@ -28,124 +31,122 @@ public class Employee {
         _rate = calculator.rate();
         _paytype = calculator.paytype();
         
-        _taxData = computePayslip();
-        _pay = new String[] {_context.getString(R.string.gross) + String.format("%.2f",_taxData[0]),_context.getString(R.string.paye) + String.format("%.2f",_taxData[1]),_context.getString(R.string.studentLoadCalc) + String.format("%.2f",_taxData[2]),_context.getString(R.string.kiwi_saver_calc) + String.format("%.2f",_taxData[3]),_context.getString(R.string.nett) + String.format("%.2f",_taxData[4])};
+        _taxCalculator = new TaxCalculator(_rate, _hours, _context);
+        
+        
+        
+//        _taxData = computePayslip();
+//        _pay = new String[] {_context.getString(R.string.gross) + String.format("%.2f",_taxData[0]),_context.getString(R.string.paye) + String.format("%.2f",_taxData[1]),_context.getString(R.string.studentLoadCalc) + String.format("%.2f",_taxData[2]),_context.getString(R.string.kiwi_saver_calc) + String.format("%.2f",_taxData[3]),_context.getString(R.string.nett) + String.format("%.2f",_taxData[4])};
 
 	}
 	
-	public String name() {
-		return null;
-	}
+//	public String name() {
+//		return null;
+//	}
+//	
+//	public String payRate() {
+//		return "";
+//	}
+//	
+//	public String hours() {
+//		return "";		
+//	}
 	
-	public String payRate() {
-		return "";
-	}
-	
-	public String hours() {
-		return "";		
-	}
-	
-	public String gross() {
-		return "" + _pay[0];
-		
+	public String gross() {		
+		return _context.getString(R.string.gross) + String.format("%.2f",_taxCalculator.get_grossIncome());
 	}
 	
 	public String paye() {
-		return "" + _pay[1];
-		
+		return _context.getString(R.string.paye) + String.format("%.2f", _taxCalculator.get_paye());
 	}
 	
-	public String studentLoan() {
-		return "" + _pay[2];
-		
+	public String studentLoan() {		
+		return _context.getString(R.string.studentLoadCalc) + String.format("%.2f",_taxCalculator.get_studentLoan());		
 	}
 	
 	public String kiwiSaver() {
-		return "" + _pay[3];
-		
+		return _context.getString(R.string.kiwi_saver_calc) + String.format("%.2f",_taxCalculator.get_kiwiSaver());		
 	}
 	
-	public String nett() {
-		return "" + _pay[4];
-		
+	public String nett() {		
+		return _context.getString(R.string.nett) + String.format("%.2f",_taxCalculator.get_nett());		
 	}
 	
-	private double[] computePayslip() {
-		// Instantiate FileReader
-		FileReader fileReader = new FileReader(_context);
-		
-        String[] preferencesArray = fileReader.readPreferences().split(",");
-        double gross = _rate*_hours;
-        String[] taxData = readTaxData().split(",");
-        double paye = 0;
-        if (preferencesArray[3].equals("M")) {
-            paye = Double.parseDouble(taxData[1]);
-        } else if (preferencesArray[3].equals("ME")) {
-            paye = Double.parseDouble(taxData[2]);
-        } else if (preferencesArray[3] .equals("ML")) {
-            paye = Double.parseDouble(taxData[3]);
-        }
-        double sL = 0;
-        if (preferencesArray[2].equals("true")) {
-            sL = Double.parseDouble(taxData[4]);
-        }
-        double kS = 0;
-        if (preferencesArray[4].equals("None")) {
-        } else if (preferencesArray[4].equals("1")){
-            kS = Double.parseDouble(taxData[5])/2.0;
-        } else if (preferencesArray[4].equals("2")) {
-            kS = Double.parseDouble(taxData[5]);
-        } else if (preferencesArray[4].equals("3")){
-            kS = Double.parseDouble(taxData[5])*(3.0/2.0);
-        } else if (preferencesArray[4].equals("4")) {
-            kS = Double.parseDouble(taxData[6]);
-        } else if (preferencesArray[4].equals("5")){
-            kS = Double.parseDouble(taxData[6])*(5.0/4.0);
-        } else if (preferencesArray[4].equals("6")){
-            kS = Double.parseDouble(taxData[6])*(6.0/4.0);
-        } else if (preferencesArray[4].equals("7")){
-            kS = Double.parseDouble(taxData[6])*(7.0/4.0);
-        } else if (preferencesArray[4].equals("8")) {
-            kS = Double.parseDouble(taxData[7]);
-        }
-        double nett = gross - paye - sL - kS;
-        double[] outputTaxData = {gross,paye,sL,kS,nett};
-
-        return outputTaxData;
-    }
-	
-	private String readTaxData() {
-        double grossIncome = _rate*_hours;
-        int readFactor;
-        InputStream is;
-        if (_paytype.equals("Weekly")) {
-            is = _context.getResources().openRawResource(R.raw.weeklypaye);
-            readFactor = 1;
-        } else {
-            is = _context.getResources().openRawResource(R.raw.fortnighlypaye);
-            readFactor = 2;
-        }
-
-        String line = "";
-        InputStreamReader isr = new InputStreamReader(is);
-        BufferedReader br = new BufferedReader(isr);
-        String prevLine;
-        if (grossIncome == 0) {
-            line = "0,0.0,0.0,0.0,0,0.0,0.0,0.0,0.0,0,0.0,0,0.0,0,0.0,0";
-
-        } else {
-            for (int i = 0; i < (int)grossIncome/readFactor; i++)
-            try {
-                prevLine = line;
-                line = br.readLine();
-                if (line == null){
-                    line = prevLine;
-                    break;
-                }
-            } catch (IOException e) {
-
-            }
-        }
-        return line;
-    }	
+//	private double[] computePayslip() {
+//		// Get the employee preferences
+//		EmployeePreferences preferences = EmployeeDatabase.getDatabase(_context).getEmployeeDetails();
+//		
+//        double gross = _rate*_hours;
+//        String[] taxData = readTaxData().split(",");
+//        double paye = 0;
+//        if (preferences.get_taxCode().equals("M")) {
+//            paye = Double.parseDouble(taxData[1]);
+//        } else if (preferences.get_taxCode().equals("ME")) {
+//            paye = Double.parseDouble(taxData[2]);
+//        } else if (preferences.get_taxCode() .equals("ML")) {
+//            paye = Double.parseDouble(taxData[3]);
+//        }
+//        double sL = 0;
+//        if (preferences.get_studentLoan().equals("true")) {
+//            sL = Double.parseDouble(taxData[4]);
+//        }
+//        double kS = 0;
+//        if (preferences.get_kiwiSaver().equals("None")) {
+//        } else if (preferences.get_kiwiSaver().equals("1")){
+//            kS = Double.parseDouble(taxData[5])/2.0;
+//        } else if (preferences.get_kiwiSaver().equals("2")) {
+//            kS = Double.parseDouble(taxData[5]);
+//        } else if (preferences.get_kiwiSaver().equals("3")){
+//            kS = Double.parseDouble(taxData[5])*(3.0/2.0);
+//        } else if (preferences.get_kiwiSaver().equals("4")) {
+//            kS = Double.parseDouble(taxData[6]);
+//        } else if (preferences.get_kiwiSaver().equals("5")){
+//            kS = Double.parseDouble(taxData[6])*(5.0/4.0);
+//        } else if (preferences.get_kiwiSaver().equals("6")){
+//            kS = Double.parseDouble(taxData[6])*(6.0/4.0);
+//        } else if (preferences.get_kiwiSaver().equals("7")){
+//            kS = Double.parseDouble(taxData[6])*(7.0/4.0);
+//        } else if (preferences.get_kiwiSaver().equals("8")) {
+//            kS = Double.parseDouble(taxData[7]);
+//        }
+//        double nett = gross - paye - sL - kS;
+//        double[] outputTaxData = {gross,paye,sL,kS,nett};
+//
+//        return outputTaxData;
+//    }
+//	
+//	private String readTaxData() {
+//        double grossIncome = _rate*_hours;
+//        int readFactor;
+//        InputStream is;
+//        if (_paytype.equals("Weekly")) {
+//            is = _context.getResources().openRawResource(R.raw.weeklypaye);
+//            readFactor = 1;
+//        } else {
+//            is = _context.getResources().openRawResource(R.raw.fortnighlypaye);
+//            readFactor = 2;
+//        }
+//
+//        String line = "";
+//        InputStreamReader isr = new InputStreamReader(is);
+//        BufferedReader br = new BufferedReader(isr);
+//        String prevLine;
+//        if (grossIncome == 0) {
+//            line = "0,0.0,0.0,0.0,0,0.0,0.0,0.0,0.0,0,0.0,0,0.0,0,0.0,0";
+//
+//        } else {
+//            for (int i = 0; i < (int)grossIncome/readFactor; i++)
+//            try {
+//                prevLine = line;
+//                line = br.readLine();
+//                if (line == null){
+//                    line = prevLine;
+//                    break;
+//                }
+//            } catch (IOException e) {
+//
+//            }
+//        }
+//        return line;
+//    }	
 }
--- src/com/mcalpinedevelopment/calculatepay/tax/TaxCalculator.java
+++ src/com/mcalpinedevelopment/calculatepay/tax/TaxCalculator.java
@@ -1,7 +1,137 @@
 package com.mcalpinedevelopment.calculatepay.tax;
 
-public class TaxCalculator {	
-	public static double gross(double hours, double rate) {
-		return hours*rate;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import android.content.Context;
+
+import com.mcalpinedevelopment.calculatepay.R;
+import com.mcalpinedevelopment.calculatepay.database.EmployeeDatabase;
+import com.mcalpinedevelopment.calculatepay.database.EmployeePreferences;
+
+public class TaxCalculator {
+
+	private String _payperiod;
+	private Context _context;
+	
+	private String _taxTableInfo;
+	
+	private double _grossIncome;
+	private double _paye;
+	private double _studentLoan;
+	private double _kiwiSaver;
+	private double _nett;
+	
+	
+	public TaxCalculator(double payRate, double hours, Context context) {
+		_context = context;
+		_grossIncome = payRate * hours;
+		_payperiod = EmployeeDatabase.getDatabase(_context).getEmployeeDetails().get_payPeriod();
+		
+		readTaxTable();
+		computePayslip();
+	}
+	
+	/**
+	 * @return Reads the line in the table containing the tax information
+	 * up to the line closest to the _grossIncome value
+	 */
+	private void readTaxTable() {
+        double grossIncome = _grossIncome;
+        int readFactor;
+        InputStream is;
+        if (_payperiod.equals("Weekly")) {
+            is = _context.getResources().openRawResource(R.raw.weeklypaye);
+            readFactor = 1;
+        } else {
+            is = _context.getResources().openRawResource(R.raw.fortnighlypaye);
+            readFactor = 2;
+        }
+
+        String line = "";
+        InputStreamReader isr = new InputStreamReader(is);
+        BufferedReader br = new BufferedReader(isr);
+        String prevLine;
+        if (grossIncome == 0) {
+            line = "0,0.0,0.0,0.0,0,0.0,0.0,0.0,0.0,0,0.0,0,0.0,0,0.0,0";
+
+        } else {
+            for (int i = 0; i < (int)grossIncome/readFactor; i++)
+            try {
+                prevLine = line;
+                line = br.readLine();
+                if (line == null){
+                    line = prevLine;
+                    break;
+                }
+            } catch (IOException e) {
+
+            }
+        }
+        _taxTableInfo = line;
+    }
+	
+	/**
+	 * Assigns the fields the values in accordence to the employees preferences and their gross income
+	 */
+	private void computePayslip() {
+		// Get the employee preferences
+		EmployeePreferences preferences = EmployeeDatabase.getDatabase(_context).getEmployeeDetails();
+		
+        String[] taxData = _taxTableInfo.split(",");
+        _paye = 0;
+        if (preferences.get_taxCode().equals("M")) {
+        	_paye = Double.parseDouble(taxData[1]);
+        } else if (preferences.get_taxCode().equals("ME")) {
+        	_paye = Double.parseDouble(taxData[2]);
+        } else if (preferences.get_taxCode() .equals("ML")) {
+        	_paye = Double.parseDouble(taxData[3]);
+        }
+        _studentLoan = 0;
+        if (preferences.get_studentLoan().equals("true")) {
+        	_studentLoan = Double.parseDouble(taxData[4]);
+        }
+        _kiwiSaver = 0;
+        if (preferences.get_kiwiSaver().equals("None")) {
+        } else if (preferences.get_kiwiSaver().equals("1")){
+        	_kiwiSaver = Double.parseDouble(taxData[5])/2.0;
+        } else if (preferences.get_kiwiSaver().equals("2")) {
+        	_kiwiSaver = Double.parseDouble(taxData[5]);
+        } else if (preferences.get_kiwiSaver().equals("3")){
+        	_kiwiSaver = Double.parseDouble(taxData[5])*(3.0/2.0);
+        } else if (preferences.get_kiwiSaver().equals("4")) {
+        	_kiwiSaver = Double.parseDouble(taxData[6]);
+        } else if (preferences.get_kiwiSaver().equals("5")){
+        	_kiwiSaver = Double.parseDouble(taxData[6])*(5.0/4.0);
+        } else if (preferences.get_kiwiSaver().equals("6")){
+        	_kiwiSaver = Double.parseDouble(taxData[6])*(6.0/4.0);
+        } else if (preferences.get_kiwiSaver().equals("7")){
+        	_kiwiSaver = Double.parseDouble(taxData[6])*(7.0/4.0);
+        } else if (preferences.get_kiwiSaver().equals("8")) {
+        	_kiwiSaver = Double.parseDouble(taxData[7]);
+        }
+        _nett = _grossIncome - _paye - _studentLoan - _kiwiSaver;
+    }
+	
+	public double get_grossIncome() {
+		return _grossIncome;
+	}
+
+	public double get_paye() {
+		return _paye;
+	}
+
+	public double get_studentLoan() {
+		return _studentLoan;
+	}
+
+	public double get_kiwiSaver() {
+		return _kiwiSaver;
+	}
+
+	public double get_nett() {
+		return _nett;
 	}
 }
