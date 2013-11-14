db5ce549dea501cd54a131f124d5bd67082af6df
Kurt McAlpine <kurtmcalpine@gmail.com>
2013-11-14 20:56:43 +1300

    Implemented key items in the about list

--- src/com/mcalpinedevelopment/calculatepay/AboutActivity.java
+++ src/com/mcalpinedevelopment/calculatepay/AboutActivity.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -13,11 +14,17 @@ import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.Toast;
 import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 
 public class AboutActivity extends Activity {
 	
 	private ListView aboutList;
 	private List<Map<String, String>> _data;
+	
+	
+	// Integer to count how many times the Developer item is clicked so the user can see an easter egg
+	private int developerClicks = 0;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +44,15 @@ public class AboutActivity extends Activity {
 	    
 	    // Email the developer
 	    datum = new HashMap<String, String>(2);
+	    datum.put("title", "Email the developer");
+	    datum.put("content", "kurt@mcalpinedevelopment.com");
+	    _data.add(datum);
+	    
+	    // Feedback
+	    datum = new HashMap<String, String>(2);
+	    datum.put("title", "Feedback");
+	    datum.put("content", "Give feedback on your experience using this application");
+	    _data.add(datum);
 		
 		SimpleAdapter adapter = new SimpleAdapter(this, _data, android.R.layout.simple_list_item_2, new String[] {"title", "content"}, new int[] {android.R.id.text1, android.R.id.text2});
 		
@@ -51,7 +67,23 @@ public class AboutActivity extends Activity {
 				// Depending on which item in the list that is clicked do different things
 				String clicked = _data.get(position).get("title");
 				if (clicked.equals("Developer")) {
-					Toast.makeText(getApplicationContext(), "Good job", Toast.LENGTH_SHORT).show();
+					developerClicks++;
+					if (developerClicks > 10) {
+						Toast.makeText(getApplicationContext(), "Well done you clicked that "+developerClicks+" times...", Toast.LENGTH_SHORT).show();
+					}
+					
+				} else if (clicked.equals("Email the developer")) {
+					Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto","kurt@mcalpinedevelopment.com", null));
+					emailIntent.putExtra(Intent.EXTRA_SUBJECT, "My Earnings");
+					startActivity(Intent.createChooser(emailIntent, ""));
+				} else if (clicked.equals("Feedback")) {
+					Uri uri = Uri.parse("market://details?id=com.mcalpinedevelopment.calculatepay");
+					Intent goToMarket = new Intent(Intent.ACTION_VIEW, uri);
+					try {
+					  startActivity(goToMarket);
+					} catch (ActivityNotFoundException e) {
+					  Toast.makeText(getBaseContext(), "Couldn't launch the market", Toast.LENGTH_LONG).show();
+					}					
 				}
 				
 		    }
