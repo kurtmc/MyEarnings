273927894880963d8e0fb7396aa97b71a33c0334
Kurt McAlpine <kurtmcalpine@gmail.com>
2013-11-15 11:26:44 +1300

    Cleaned up the code in AboutActivity

--- src/com/mcalpinedevelopment/calculatepay/AboutActivity.java
+++ src/com/mcalpinedevelopment/calculatepay/AboutActivity.java
@@ -19,6 +19,18 @@ import android.content.Intent;
 
 public class AboutActivity extends Activity {
 	
+	// Strings used in list
+	private final static String TITLE = "title";
+	private final static String CONTENT = "content";
+	
+	private final static String DEV = "Developer";
+	private final static String DEV_C = "Kurt McAlpine";
+	private final static String EMAIL = "Email the developer";
+	private final static String EMAIL_C = "kurt@mcalpinedevelopment.com";
+	private final static String FEEDBACK = "Feedback";
+	private final static String FEEDBACK_C = "Give feedback on your experience using this application";
+	
+	
 	private ListView aboutList;
 	private List<Map<String, String>> _data;
 	
@@ -38,23 +50,23 @@ public class AboutActivity extends Activity {
 	    Map<String, String> datum = new HashMap<String, String>(2);
 	    
 	    // Developer name
-	    datum.put("title", "Developer");
-	    datum.put("content", "Kurt McAlpine");
+	    datum.put(TITLE, DEV);
+	    datum.put(CONTENT, DEV_C);
 	    _data.add(datum);
 	    
 	    // Email the developer
 	    datum = new HashMap<String, String>(2);
-	    datum.put("title", "Email the developer");
-	    datum.put("content", "kurt@mcalpinedevelopment.com");
+	    datum.put(TITLE, EMAIL);
+	    datum.put(CONTENT, EMAIL_C);
 	    _data.add(datum);
 	    
 	    // Feedback
 	    datum = new HashMap<String, String>(2);
-	    datum.put("title", "Feedback");
-	    datum.put("content", "Give feedback on your experience using this application");
+	    datum.put(TITLE, FEEDBACK);
+	    datum.put(CONTENT, FEEDBACK_C);
 	    _data.add(datum);
 		
-		SimpleAdapter adapter = new SimpleAdapter(this, _data, android.R.layout.simple_list_item_2, new String[] {"title", "content"}, new int[] {android.R.id.text1, android.R.id.text2});
+		SimpleAdapter adapter = new SimpleAdapter(this, _data, android.R.layout.simple_list_item_2, new String[] {TITLE, CONTENT}, new int[] {android.R.id.text1, android.R.id.text2});
 		
 		aboutList.setAdapter(adapter);
 		
@@ -62,21 +74,24 @@ public class AboutActivity extends Activity {
 		aboutList.setOnItemClickListener(new OnItemClickListener() {
 			@Override
 		    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-				// When clicked, show a toast with the TextView text
-				
 				// Depending on which item in the list that is clicked do different things
-				String clicked = _data.get(position).get("title");
-				if (clicked.equals("Developer")) {
+				String clicked = _data.get(position).get(TITLE);
+				
+				// DEV clicked
+				if (clicked.equals(DEV)) {
 					developerClicks++;
 					if (developerClicks > 10) {
 						Toast.makeText(getApplicationContext(), "Well done you clicked that "+developerClicks+" times...", Toast.LENGTH_SHORT).show();
 					}
 					
-				} else if (clicked.equals("Email the developer")) {
+				// EMAIL clicked
+				} else if (clicked.equals(EMAIL)) {
 					Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto","kurt@mcalpinedevelopment.com", null));
 					emailIntent.putExtra(Intent.EXTRA_SUBJECT, "My Earnings");
 					startActivity(Intent.createChooser(emailIntent, ""));
-				} else if (clicked.equals("Feedback")) {
+				
+				// FEEDBACK clicked
+				} else if (clicked.equals(FEEDBACK)) {
 					Uri uri = Uri.parse("market://details?id=com.mcalpinedevelopment.calculatepay");
 					Intent goToMarket = new Intent(Intent.ACTION_VIEW, uri);
 					try {
@@ -85,11 +100,7 @@ public class AboutActivity extends Activity {
 					  Toast.makeText(getBaseContext(), "Couldn't launch the market", Toast.LENGTH_LONG).show();
 					}					
 				}
-				
-		    }
-
-			
-		  });
+			}
+		});
 	}
-
 }
